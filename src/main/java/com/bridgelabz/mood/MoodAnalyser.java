71113a482a52f28d5ea1b7f6e75d8beccd00cf6a
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-23 23:26:42 +0530

    Exception Hendel

--- src/main/java/com/bridgelabz/mood/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/mood/MoodAnalyser.java
@@ -4,13 +4,21 @@ package com.bridgelabz.mood;
 public class MoodAnalyser {
     String message;
 
-    public static String analyzeMood(String message) {
-        if (message.contains("Sad")) {
-            return "SAD";
-        } else if (message.contains("Happy")) {
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood(String sad) {
+        try {
+
+            if (this.message.contains(null))
+                return "HAPPY";
+            else
+                return "SAD";
+        } catch (NullPointerException e) {
             return "HAPPY";
-        } else {
-            return null;
         }
     }
 }
+
+
