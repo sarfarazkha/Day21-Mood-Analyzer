7d883a6fd6059837232a21015e28fd55ff010bbd
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-23 23:55:18 +0530

    return sad

--- src/main/java/com/bridgelabz/mood/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/mood/MoodAnalyser.java
@@ -2,34 +2,27 @@ package com.bridgelabz.mood;
 
 
 public class MoodAnalyser {
-    private String message;
+    String message;
 
-    public MoodAnalyser(String i_am_in_happy_mood) {
+    public static String analyzeMood(String message) {
+        if (message.contains("sad")) {
+            return "SAD";
+        } else if (message.contains("Happy")) {
+            return "HAPPY";
+        } else {
+            return null;
+        }
     }
-
-    public void MoodAnalyse() {
-    }
-
-    public void MoodAnalyzer(String message) {
-        this.message = message;
-    }
-
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-
-            if (this.message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (Exception e) {
-            if (message == null)
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
-            else
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
+    public static String analyzeMood2(String message) {
+        if (message.contains("i am in sad mood")) {
+            return "SAD";
+        } else if (message.contains("Happy")) {
+            return "HAPPY";
+        } else {
+            return null;
         }
     }
 }
-
 
 
 
--- src/test/java/com/bridgelabz/moodtest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodtest/MoodAnalyserTest.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.moodtest;
 
 
+
 import com.bridgelabz.mood.MoodAnalyser;
-import com.bridgelabz.mood.MoodAnalysisException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,32 +10,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyserTest extends MoodAnalyser {
-    public MoodAnalyserTest(String i_am_in_happy_mood) {
-        super(i_am_in_happy_mood);
+    @Test
+    void givenMessageWhenSadShouldReturnSad() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String result = MoodAnalyser.analyzeMood("sad");
+        assertEquals("SAD", result);
     }
 
     @Test
-    public void givenMessage_SadMood_Should_Return_Sad() {
-        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am In a Sad Mood");
-        String mood;
-        try {
-            mood = moodAnalyzer.analyseMood();
-            assertEquals("SAD", mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void givenMessage_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-        String mood;
-        try {
-            mood = moodAnalyser.analyseMood();
-            assertEquals("HAPPY", mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
+    void givenMessageWhenIaminsadmoodShouldReturnSad() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String result = MoodAnalyser.analyzeMood2("i am in sad mood");
+        assertEquals("SAD", result);
     }
 }
-
