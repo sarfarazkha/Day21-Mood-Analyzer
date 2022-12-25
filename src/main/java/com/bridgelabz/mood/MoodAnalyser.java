ae88fa8083133ff4e5ced997a51e810b76d9a298
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-12-25 22:07:36 +0530

    Added: ExceptionHandle-invalid

--- src/main/java/com/bridgelabz/mood/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/mood/MoodAnalyser.java
@@ -7,7 +7,7 @@ public class MoodAnalyser {
     protected static String analyzeMood3(String i_am_in_any_mood) {
     }
 
-    public String analyseMood(String message) {
+    public String analyseMood(String message) { 
         try {
             if (message.contains("Sad")) {
                 return "SAD";
